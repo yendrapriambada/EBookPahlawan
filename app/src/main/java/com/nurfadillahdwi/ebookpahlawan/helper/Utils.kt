4ce3609286489a58a7d19963c74670326263a007
yendra priambada <yendrapriambada1234@gmail.com>
2023-04-11 20:59:21 +0700

    Revisi Layout Practice (add keterangan edittext) & Add Intent Gallery

--- app/src/main/java/com/nurfadillahdwi/ebookpahlawan/helper/Utils.kt
+++ app/src/main/java/com/nurfadillahdwi/ebookpahlawan/helper/Utils.kt
@@ -1,9 +1,19 @@
 package com.nurfadillahdwi.ebookpahlawan.helper
 
-
 import android.app.Activity
+import android.content.ContentResolver
+import android.content.Context
+import android.net.Uri
+import android.os.Environment
 import android.widget.Toast
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStream
+import java.io.OutputStream
+import java.text.SimpleDateFormat
+import java.util.*
 
+private const val FILENAME_FORMAT = "dd-MMM-yyyy"
 
 fun setupToken(token: String): String {
     return "Bearer $token"
@@ -13,4 +23,28 @@ fun showToast(context: Activity, message: String) {
     Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
 }
 
+fun uriToFile(selectedImg: Uri, context: Context): File {
+    val contentResolver: ContentResolver = context.contentResolver
+    val myFile = createTempFile(context)
+
+    val inputStream = contentResolver.openInputStream(selectedImg) as InputStream
+    val outputStream: OutputStream = FileOutputStream(myFile)
+    val buf = ByteArray(1024)
+    var len: Int
+    while (inputStream.read(buf).also { len = it } > 0) outputStream.write(buf, 0, len)
+    outputStream.close()
+    inputStream.close()
+
+    return myFile
+}
+
+fun createTempFile(context: Context): File {
+    val storageDir: File? = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+    return File.createTempFile(timeStamp, ".jpg", storageDir)
+}
+
+val timeStamp: String = SimpleDateFormat(
+    FILENAME_FORMAT,
+    Locale.US
+).format(System.currentTimeMillis())
 
--- app/src/main/java/com/nurfadillahdwi/ebookpahlawan/view/PracticeActivity.kt
+++ app/src/main/java/com/nurfadillahdwi/ebookpahlawan/view/PracticeActivity.kt
@@ -1,12 +1,51 @@
 package com.nurfadillahdwi.ebookpahlawan.view
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import com.nurfadillahdwi.ebookpahlawan.R
+import com.nurfadillahdwi.ebookpahlawan.databinding.ActivityMainBinding
+import com.nurfadillahdwi.ebookpahlawan.databinding.ActivityPracticeBinding
+import com.nurfadillahdwi.ebookpahlawan.helper.uriToFile
+import java.io.File
 
 class PracticeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityPracticeBinding
+    private var getFile: File? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_practice)
+        binding = ActivityPracticeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.addPhoto.setOnClickListener {
+            startGallery()
+        }
+    }
+
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = Intent.ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Pilih Gambar Pahlawan")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg: Uri = result.data?.data as Uri
+
+            val myFile = uriToFile(selectedImg, this@PracticeActivity)
+
+            getFile = myFile
+
+            binding.addPhoto.setImageURI(selectedImg)
+        }
     }
 }
